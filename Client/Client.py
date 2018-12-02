f4d272b019658870caff5cc18e094a50acb0b62e
aval984 <andrew.f.valenzuela@gmail.com>
2018-12-01 16:14:03 -0800

    stuff

--- Client/Client.py
+++ Client/Client.py
@@ -7,53 +7,70 @@ import random
 import Auction
 
 class Client:
-
+    
     # Recommend referring to this site for basic intro: https://www.geeksforgeeks.org/socket-programming-python/
     # Recommend installing Python plugins for VS Code. The intellisense is awesome
-
-    def __init__(self, newBalance):
+    
+    def __init__(self, serverIP, newBalance):
         self._balance = newBalance      # the amount of money this client has
         self._activeAuction = None      # this Will be used to determine which auction the client is engaged in. Recommend a Tuple of (GUID, Auction). GUID is passed from the server
         self._inventory = list()        # list containing the items this client has won. Can use Item.AddUnit() to increase the number of units of the item
         self._server = None             # Server socket the client is connected to
-        self.ConnectToServer()
-
-    def ConnectToServer(self):
+        self.ConnectToServer(serverIP)
+    
+    def ConnectToServer(self, serverIP):
         # this function should connect the client to the server.
-        
         self._server = socket.socket()
-        self._server.connect(("127.0.0.1", 12345))   # connect to localhost on port 12345, which is specified in server file
-
+        self._server.connect((serverIP, 12345))   # connect to localhost on port 12345, which is specified in server file
     def ClientLoop(self):
         # This is the main execution loop for the client that's called from main(). Some example code is shown below.
         # The loop should run a series of Functions that are defined in this class. Recommend having a look at Server.ServerLoop() for an example
-
-        while True:                           # Infinite Loop
-            data = self._server.recv(4096)    # This listens for data from the server. Program execution is blocked here until data is received
-
-            # print(data)
+        
+        while True:     # Infinite Loop
+            amountrecv = 0
+            packetsize = int(self._server.recv(1024))
+            
+            self._server.send("receivedSize")
+            print(packetsize)
+            data = ""
+            while amountrecv < packetsize:
+                print(amountrecv)
+                
+                rec = self._server.recv(1024)    # This listens for data from the server. Program execution is blocked here until data is received
+                
+                
+                amountrecv += len(rec)
+                #if amountrecv < packetsize:
+                #self._server.send("notReceived")
+                data+=rec
+            # print(sys.getsizeof(data))
+            print(amountrecv)
+            
+            self._server.send("received")
             dataDecomp = pickle.loads(data)   # This decompresses the data sent from the server. This allows us to get a Tuple object from the server.
-
+            #return
+            print(dataDecomp[0])
+            continue
             # The server sends tuples in the form of ("Message", data)
             if dataDecomp[0] == "AuctionWon":
                 # If server sends "AuctionWon" message, it will contain Tuple data (Item, finalBidPrice)
                 print("Won " + dataDecomp[1][0].GetName())
-
-            elif dataDecomp[0] == "NewRound":
-                # If server sends "NewRound" message, it will contain a dictionary of auction items
-                # The dictionary has keys of GUID strings with values of Auction type.
-                # You'll probably want to keep track of the client's active auction's GUID.
-                auctionChoice = random.choice(list(dataDecomp[1])) # randomly picks an auction to go for
-                dataToSend = pickle.dumps((auctionChoice, 3999))   # sends a bid to the auction for $3999
-                self._server.send(dataToSend)                      # sends the bid to the server. Server handles the rest
-
+    
+    elif dataDecomp[0] == "NewRound":
+        # If server sends "NewRound" message, it will contain a dictionary of auction items
+        # The dictionary has keys of GUID strings with values of Auction type.
+        # You'll probably want to keep track of the client's active auction's GUID.
+        auctionChoice = random.choice(list(dataDecomp[1])) # randomly picks an auction to go for
+        dataToSend = pickle.dumps((auctionChoice, 3999))   # sends a bid to the auction for $3999
+        self._server.sendall(dataToSend)                      # sends the bid to the server. Server handles the rest
+    
     def JoinAuction(self):
         # This function should first check if this client has an active auction or not.
         # If we don't have an active auction, then we want to receive the list of auctions from the server and let the client pick one
         # The server sends a list of auctions in this format:
-            # ("NewRound", auctionDictionary)
-            # auctionDictionary has GUID strings as a key and Auction class objects as types.
-            # You'll want to save the GUID of the auction that the client chooses. You'll need it to send a bid to the server.
+        # ("NewRound", auctionDictionary)
+        # auctionDictionary has GUID strings as a key and Auction class objects as types.
+        # You'll want to save the GUID of the auction that the client chooses. You'll need it to send a bid to the server.
         
         return
     
@@ -63,9 +80,9 @@ class Client:
         # Client should send None if it is not bidding on an item
         # According to Ming Li, the client has a 30% chance of NOT bidding
         # Client should send a tuple (auctionGUID, bidAmount)
-
+        
         return
-
+    
     def GetWonItem(self):
         # this function should listen for the server to send an item to this client upon winning a bid.
         # The item will be sent as an (Item, finalBidPrice) tuple
@@ -76,10 +93,13 @@ def main():
     # implement main client execution here. I imagine this is for a single client.
     # We can do multiple clients by opening multiple consoles and running python Client.py
     # could also put Client.py, Item.py, and Auction.py on another computer and run Client.py. That should work on a LAN.
-
-    testClient = Client(150)    # client has $150 balance
-    testClient.ClientLoop()
+    if len(sys.argv) < 2:
+        print("Error: Please put in an IP address for the server. E.G: python Client.py 192.168.1.1")
+        exit()
     
+    testClient = Client(sys.argv[1], 150)    # client has $150 balance
+    testClient.ClientLoop()
+
 if __name__ == "__main__":
     # call main when this program is run. This if statement ensures this code is not run if imported as a module
     main()
